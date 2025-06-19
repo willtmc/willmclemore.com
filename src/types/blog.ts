3f447515b34ae8043a0fe90f137586f6ad13af3e
willtmc <will@mclemoreauction.com>
2025-06-18 21:56:39 -0500

    fix: export BlogPost type from types/blog module
    - Re-export BlogPost type from @prisma/client
    - Fix import error in blog-content.ts

--- src/types/blog.ts
+++ src/types/blog.ts
@@ -1,4 +1,7 @@
-import { BlogPost } from '@prisma/client'
+import { BlogPost as PrismaBlogPost } from '@prisma/client'
+
+// Re-export BlogPost type from Prisma
+export type BlogPost = PrismaBlogPost
 
 export interface BlogPostWithMetadata extends BlogPost {
   readingTime: number
